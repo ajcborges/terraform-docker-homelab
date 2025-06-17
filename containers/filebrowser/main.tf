1807918ff73070af7cbd7744b6f31e3c895cbc1c
alfredo cedeno <ajcborges@gmail.com>
2025-06-17 16:29:05 +1000

    deploy FileBrowser Container

---
+++ containers/filebrowser/main.tf
@@ -0,0 +1,44 @@
+module "container" {
+  source = "git::https://github.com/ajcborges/terraform-modules.git//container?ref=ba73b21"
+
+  image          = "filebrowser/filebrowser:latest"
+  container_name = "filebrowser2"
+  hostname       = "filebrowser"
+  restart_policy = "unless-stopped"
+  environment = {
+    PUID = "1000"
+    GUID = "1000"
+    TZ   = "Australia/Melbourne"
+    FILEBROWSER_BASEURL = "/"
+    FILEBROWSER_DATABASE = "/data/filebrowser.db"
+    FILEBROWSER_ROOT = "/mnt/usb2tb/containers/filebrowser/"
+    FILEBROWSER_AUTH_METHOD = "password"
+    FILEBROWSER_AUTH_USERNAME = "admin"
+    FILEBROWSER_AUTH_PASSWORD = "admin"
+    FILEBROWSER_AUTH_PASSWORD_CONFIRM = "admin"
+    FILEBROWSER_AUTH_DEFAULT_PERMISSIONS = "rwxr-xr-x"
+    FILEBROWSER_AUTH_DEFAULT_PERMISSIONS_DIR = "rwxr-xr-x"
+    FILEBROWSER_AUTH_DEFAULT_PERMISSIONS_FILE = "rw-r--r--"
+    FILEBROWSER_AUTH_DEFAULT_PERMISSIONS_SYMLINK = "rwxr-xr-x"
+    FILEBROWSER_AUTH_DEFAULT_PERMISSIONS_SOCKET = "rwxr-xr-x"
+  }
+  ports = [{
+    external = "8001"
+    internal = "80"
+    protocol = "tcp"
+  }]
+
+
+  host_paths = {
+    "/mnt/usb2tb/containers/filebrowser/data" = {
+      container_path = "/data/"
+      read_only      = false
+    },
+    "/mnt/usb2tb/" = {
+      container_path = "/srv"
+      read_only      = true
+
+    }
+
+  }
+}
---
+++ containers/filebrowser/version.tf
@@ -0,0 +1,25 @@
+#################################
+# Terraform Version & Providers.
+#################################
+
+terraform {
+  required_version = ">= 1.3.3"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.4.0"  # Allow patch updates like 3.4.x
+    }
+  }
+
+  backend "gcs" {
+    bucket  = "tf-homelab-backend"
+    prefix  = "containers/filebrowser/tfstate"
+  }
+}
+
+# Docker provider config
+provider "docker" {
+  host     = "ssh://ajcborges@whitewalker:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
