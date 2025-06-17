0b33c2e5710c605745f196a6131bd9393c34bc2b
Alfredo Cedeno <ajcborges@gmail.com>
2025-06-17 15:26:08 +1000

    Update homepage.tf

--- containers/homepage/homepage.tf
+++ containers/homepage/homepage.tf
@@ -9,10 +9,10 @@ module "container" {
     PUID = "1000"
     GUID = "1000"
     TZ   = "Australia/Melbourne"
-    HOMEPAGE_ALLOWED_HOSTS = "whitewalker:3007"
+    HOMEPAGE_ALLOWED_HOSTS = "whitewalker:3008"
   }
   ports = [{
-    external = "3007"
+    external = "3008"
     internal = "3000"
     protocol = "tcp"
   }]
