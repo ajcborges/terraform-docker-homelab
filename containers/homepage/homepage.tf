fb3fbdc988c4278b0ec7739d6170911a009c78e0
alfredo cedeno <ajcborges@gmail.com>
2025-07-22 14:07:33 +1000

    update module ref

--- containers/homepage/homepage.tf
+++ containers/homepage/homepage.tf
@@ -1,5 +1,5 @@
 module "container" {
-  source = "git::https://github.com/ajcborges/terraform-modules.git//container?ef=container/2.0.0"
+  source = "git::https://github.com/ajcborges/terraform-modules.git//container?ref=container/2.0.0"
 
   image          = "ghcr.io/gethomepage/homepage:v1.3.2"
   container_name = "homepage"
