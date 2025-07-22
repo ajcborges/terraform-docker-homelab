9c4f9b4c75792c5a39fd296932147ea41ccc2f87
ajcborges <alfredo@cedeno.me>
2025-07-22 12:21:48 +1000

    tesing  mariadb environment

--- containers/mariadb/variables.tf
+++ containers/mariadb/variables.tf
@@ -171,18 +171,3 @@ EOD
   default     = []
 }
 
-variable "mysql_root_password" {
-  type = string
-}
-
-variable "mysql_database" {
-  type = string
-}
-
-variable "mysql_user" {
-  type = string
-}
-
-variable "mysql_password" {
-  type = string
-}
