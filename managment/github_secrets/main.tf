ea600390e4f1e2c47a2a70d2b79e1857488da860
Alfredo Cedeno <ajcborges@gmail.com>
2025-08-04 14:26:15 +1000

    Delete managment directory

--- managment/github_secrets/main.tf
+++
@@ -1,5 +0,0 @@
-resource "github_actions_secret" "homelab_container_secrets" {
-  repository      = var.github_repository
-  secret_name     = var.secret_name
-  plaintext_value = var.secret_value
-}
