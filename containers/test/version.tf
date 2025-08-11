5620561a0f3b7955f5c1ac0003e582bb44807643
Alfredo Cedeno <ajcborges@gmail.com>
2025-08-12 09:16:29 +1000

    Create version.tf

---
+++ containers/test/version.tf
@@ -0,0 +1,25 @@
+#################################
+# Terraform Version & Providers.
+#################################
+
+terraform {
+  required_version = ">= 1.3.3"
+
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.6.2"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "tf-homelab-backend"
+    prefix = "containers/codeserver/tfstate/production.tfstate"
+  }
+}
+
+# Docker provider config
+provider "docker" {
+  host     = "ssh://ajcborges@nightking:22"
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+}
